5a0de409881d0872893fbfa25a1b80b78fc4c0c7
Hergio88 <sergio.g2388@gmail.com>
2023-12-04 09:34:48 -0300

    se corrije sintaxis

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /*!
  * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2022 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
-(() => {
+ * Copyright 2011-2022 The Bootstrap Authors  */
+ (() => {
     "use strict";
   
     const storedTheme = localStorage.getItem("theme");
